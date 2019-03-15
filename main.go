9c2dea28ed90983a837bcc58afbcda9174fcd023
Dustin Belliston <dustinbelliston@gmail.com>
2019-03-15 15:06:42 -0600

    api path

--- main.go
+++ main.go
@@ -70,9 +70,9 @@ func main() {
 	router := mux.NewRouter()
 	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	router.HandleFunc("/people", GetPeople).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	router.HandleFunc("/api/people", GetPeople).Methods("GET")
+	router.HandleFunc("/api/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/api/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/api/people/{id}", DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
